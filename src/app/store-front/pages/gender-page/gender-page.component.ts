21f33a1ba073c77fd09b27661f01a0077cc60fb9
Uriel Mora <upm1808@gmail.com>
2025-04-23 13:18:26 -0600

    Paginacion en generos

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -4,24 +4,28 @@ import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
 import { ProductService } from '../../../products/services/product.service';
 import { ProductCardComponent } from "../../../products/components/product-card/product-card.component";
+import { PaginationComponent } from "../../../shared/components/pagination/pagination.component";
+import { PaginationService } from '../../../shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './gender-page.component.html',
 })
 export class GenderPageComponent {
 
 route = inject(ActivatedRoute);
 ProductService = inject(ProductService);
+paginationServices = inject(PaginationService);
 
 gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
 
   productResource = rxResource({
-    request: () => ({ gender: this.gender()}),
+    request: () => ({ gender: this.gender(), page: this.paginationServices.currentPage() -  1 }),
     loader:({ request}) => {
       return this.ProductService.getProduct({
         gender: request.gender,
+        offset: request.page * 8
       });
     },
   });
