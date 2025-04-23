2020198099b5e9c2a502d9321f45688d5f5dc706
Uriel Mora <upm1808@gmail.com>
2025-04-23 11:30:43 -0600

    Paginacion local

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from "../../../products/components/product-card/product-card.component";
 import { ProductService } from '../../../products/services/product.service';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { PaginationComponent } from "../../../shared/components/pagination/pagination.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
