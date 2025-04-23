7586062df085a0bdb2430ca16fa94c758ac38b8c
Uriel Mora <upm1808@gmail.com>
2025-04-23 12:42:19 -0600

    Paginacion con RxResource

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ProductCardComponent } from "../../../products/components/product-card/product-card.component";
 import { ProductService } from '../../../products/services/product.service';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { PaginationComponent } from "../../../shared/components/pagination/pagination.component";
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -13,10 +15,24 @@ export class HomePageComponent {
 
 ProductService = inject(ProductService);
 
+activateRoute = inject(ActivatedRoute);
+
+currentPage = toSignal(
+  this.activateRoute.queryParamMap.pipe(
+    map( params => (params.get('page') ? +params.get('page') ! : 1)),
+    map((page) => (isNaN(page) ? 1 : page))
+
+  ),
+  {
+    initialValue: 1,
+  }
+);
+
 productResource = rxResource({
-  request: () => ({}),
+  request: () => ({ page: this.currentPage() - 1}),
   loader:({ request}) => {
     return this.ProductService.getProduct({
+      offset: request.page * 8
 
     });
   },
