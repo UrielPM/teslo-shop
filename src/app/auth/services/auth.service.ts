f41fdec0322a1da42216aa8d5a1ae95131709b27
Uriel Mora <upm1808@gmail.com>
2025-04-28 17:15:10 -0600

    feat: checkStatus AuthService

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { AuthResponse } from '../interfaces/auth-response.interface';
 import { catchError, map, Observable, of, tap } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
 const baseUrl = environment.baseUrl;
@@ -15,6 +16,12 @@ export class AuthService {
   private _user = signal<User | null>(null);
   private _token = signal<string | null>(null);
   private http = inject(HttpClient);
+
+
+  checkStatusResource = rxResource({
+    loader:() => this.checkStatus()
+
+  })
 
   authStatus = computed<AuthStatus>(() => {
     if (this._authStatus() === 'checking') return 'checking';
@@ -48,5 +55,34 @@ export class AuthService {
           return of(false);
       })
     );
+  }
+
+  checkStatus(): Observable<boolean> {
+
+    const token = localStorage.getItem('token');
+      if( !token){
+        return of(false)
+      }
+      return this.http.get<AuthResponse>(`${baseUrl}/auth/check-status`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+      }).pipe(
+        tap( resp => {
+          this._user.set(resp.user);
+          this._authStatus.set('authenticated');
+          this._token.set(resp.token);
+
+          localStorage.setItem('token', resp.token);
+        }),
+        map(() => true),
+        catchError((eror: any) => {
+          this._user.set(null);
+          this._token.set(null);
+          this._authStatus.set('not-authenticated');
+            return of(false);
+        })
+      );
+
   }
 }
