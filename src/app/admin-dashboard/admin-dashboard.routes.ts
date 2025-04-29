1bf9c37f4a21b74cc338ac12ca26be331a0452e9
Uriel Mora <upm1808@gmail.com>
2025-04-29 16:03:35 -0600

    feat: struture admin-pages y layouts and routes

--- src/app/admin-dashboard/admin-dashboard.routes.ts
+++ src/app/admin-dashboard/admin-dashboard.routes.ts
@@ -1,8 +1,26 @@
 import {  Routes } from "@angular/router";
+import { AdminDashboardLayoutComponent } from "./layouts/admin-dashboard-layout/admin-dashboard-layout.component";
+import { ProductAdminPageComponent } from "./pages/product-admin-page/product-admin-page.component";
 
 export const adminDashboardRoutes: Routes = [
   {
+    path: '',
+    component: AdminDashboardLayoutComponent,
+    children:[
+      {
+        path: 'products',
+        component: ProductAdminPageComponent
+      },
+      {
+        path: 'product/:id',
+        component: ProductAdminPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'products',
+      }
 
+    ]
   }
 ];
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,21 +2,23 @@ import { Routes } from '@angular/router';
 import { NotAuthenticatedGuard } from './auth/guards/not-authenticated.guard';
 
 export const routes: Routes = [
-{
-  path: 'auth',
-  loadChildren:() => import('./auth/auth.routes'),
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
     canMatch: [
       NotAuthenticatedGuard,
       //() => {
-       // console.log('Hola mundo');
-        //return true;
+      // console.log('Hola mundo');
+      //return true;
       //},
     ],
-},
-
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin-dashboard/admin-dashboard.routes'),
+  },
   {
     path: '',
     loadChildren: () => import('./store-front/store-front.route'),
   },
-
 ];
